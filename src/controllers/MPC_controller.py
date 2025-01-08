9542ac06912aefc5a4b1ecbf7c1dae500a1fc67b
Jackmastro <giacomma@student.ethz.ch>
2025-01-08 12:04:00 +0100

    mpc ready for debugging

--- src/controllers/MPC_controller.py
+++ src/controllers/MPC_controller.py
@@ -1,43 +1,66 @@
 from .controller_base import ControllerBase
 import numpy as np
 import cvxpy as cp
+from classes import Model
 
-class MPController(ControllerBase):
-    def __init__(self, model, Q:np.ndarray, S:np.ndarray, R:np.ndarray, pred_time:int, sampling_time:int, discret_time:int, print_output:bool=False, verbose:bool=False) -> None:
-        # self.setpoint = setpoint
+class MPCController(ControllerBase):
+    def __init__(self, model:Model, T_top_ref:float, T_cell_ref:float,
+                 A_d:np.ndarray, B_d:np.ndarray, h_d:np.ndarray, Q:np.ndarray, S:np.ndarray, R:np.ndarray,
+                 pred_time:int, sampling_time:int, discret_time:int,
+                 print_output:bool=False, verbose:bool=False) -> None:
+        
+        self.setpoint = T_cell_ref # for plotting
+
+        self.x_inf = np.array([1,
+                               T_top_ref,
+                               T_top_ref]) # TODO check if soc and bot make sense
         
         self.Q = Q
         self.S = S
         self.R = R
-        self.pred_time = pred_time
-        self.sampling_time = sampling_time
-        self.discret_time = discret_time
+        self.pred_time = pred_time         # s: prediction horizon
+        self.sampling_time = sampling_time # s: sampling time for the calculation of control inputs
+        self.discret_time = discret_time   # s: discretization time for the model evolution
 
         self.K = int(pred_time / discret_time)
 
-        self.n = 3 # number of states
-        self.m = 2 # number of inputs
+        self.n = A_d.shape[0] # number of states
+        self.m = B_d.shape[1] # number of inputs
 
-        self.A, self.B, self.h, _, _, _ = model.get_linearization()
+        self.A_d, self.B_d, self.h_d = A_d, B_d, h_d
+
+        self.model = model
 
         self.print_output = print_output
         self.verbose = verbose
         self.num_infeas = 0
         self.num_None_output = 0
 
+        self._init_constraints()
+
         self._init_optimization_problem()
+
+        # Initialize open-loop input
+        self.open_loop_u = np.zeros((self.m, self.K))
+
+    def _init_constraints(self) -> None:
+        # I_HP constraints
+        I_HP_min_U_BT, I_HP_max_U_BT = self.model.get_constraints_U_BT2I_HP(self.x_inf[2] - self.x_inf[1]) # TODO distinguere se scaldando o raffreddando
+        # I_HP_min_I_BT, I_HP_max_I_BT = self.model.get_constraints_I_BT2I_HP(u_bounded)
+        self.I_min = max(I_HP_min_U_BT, self.model.I_HP_min)
+        self.I_max = min(I_HP_max_U_BT, self.model.I_HP_max)
 
     def _init_optimization_problem(self) -> None:
         # Define variables and dynamic parameters for the optimization problem
-        self.x_var          = cp.Variable((self.n, self.K+1))
-        self.i_var          = cp.Variable((self.m, self.K))
-        self.x0_param       = cp.Parameter(self.n)
+        self.x_var    = cp.Variable((self.n, self.K+1))
+        self.i_var    = cp.Variable((self.m, self.K))
+        self.x0_param = cp.Parameter(self.n)
 
         # Placeholders for parametrization
-        self.A_d = cp.Parameter((self.n, self.n))
-        self.B_d = cp.Parameter((self.n, self.m))
-        self.h_d = cp.Parameter(self.n)
-        self.xss = cp.Parameter(self.n)
+        # self.A_d   = cp.Parameter((self.n, self.n)) # maybe not needed: only for online linearization
+        # self.B_d   = cp.Parameter((self.n, self.m))
+        # self.h_d   = cp.Parameter(self.n)
+        # self.x_inf = cp.Parameter(self.n)
 
         # Initialize cost and constraints (parametrized)
         self.cost = 0
@@ -46,22 +69,22 @@ class MPController(ControllerBase):
         # Iterate over the horizon K - 1 for cost and constraints
         for k in range(self.K):
             self.cost += (
-                cp.quad_form(self.x_var[:, k] - self.xss, self.Q) + 
-                cp.quad_form(self.i_var[k], self.R)
+                cp.quad_form(self.x_var[:, k] - self.x_inf, self.Q) + 
+                cp.quad_form(self.i_var[:, k], self.R)
                 )
             self.constraints += [
-                self.x_var[:, k+1] == self.A_d @ self.x_var[:, k] + self.B_d * self.i_var[k] + self.h_d,
-                self.i_var[0, k]   >= - self.model.I_HP_max,
-                self.i_var[0, k]   <= self.model.I_HP_max,
-                self.i_var[1, k]   >= 0,
+                self.x_var[:, k+1] == self.A_d @ self.x_var[:, k] + self.B_d @ self.i_var[:, k] + self.h_d,
+                self.i_var[0, k]   >= self.I_min, # Peltier
+                self.i_var[0, k]   <= self.I_max,
+                self.i_var[1, k]   >= 0, # fan
                 self.i_var[1, k]   <= 1,
-                self.x_var[0, k]   >= 0,
+                self.x_var[0, k]   >= 0, # SoC
                 self.x_var[0, k]   <= 1,
                 ]
         
         # Terminal cost or constraint for stability
-        self.cost += cp.quad_form(self.x_var[:, self.K] - self.xss, self.S)
-        # self.constraints += [self.x_var[:, self.K_d] == self.xss]
+        self.cost += cp.quad_form(self.x_var[:, self.K] - self.x_inf, self.S)
+        # self.constraints += [self.x_var[:, self.K] == self.x_inf]
 
         self.problem = cp.Problem(cp.Minimize(self.cost), self.constraints)
 
@@ -69,26 +92,23 @@ class MPController(ControllerBase):
             print("Is the problem DPP compliant?", self.problem.is_dpp())
             print("Is the problem DCP compliant?", self.problem.is_dcp())
 
-    def _update_linearized_matrices(self, A:np.ndarray, B:np.ndarray, h:np.ndarray) -> None:
-        self.A_d.value = A
-        self.B_d.value = B
-        self.h_d.value = h
+    # def _update_linearized_matrices(self, A_d:np.ndarray, B_d:np.ndarray, h_d:np.ndarray) -> None:
+    #     self.A_d.value = A_d
+    #     self.B_d.value = B_d
+    #     self.h_d.value = h_d
 
-    def get_control_input(self, x0, time_index, xss, dss) -> tuple:
+    def get_control_input(self, current_time:float, x:np.ndarray, y:np.ndarray) -> np.ndarray:
         if self.print_output:
-            print(f"--  Simulation time: {time_index} min  -  Infeasible solutions: {self.num_infeas} of which None output: {self.num_None_output}  --")
-        
-        # Save uss and xss at the beginning of the simulation
-        if time_index == 0:
-            self.xss.value = xss
+            print(f"--  Simulation time: {current_time} min  -  Infeasible solutions: {self.num_infeas} of which None output: {self.num_None_output}  --")
 
-            # Initialize the open-loop input
-            dss.basal_handler_params['open-loop input'] = np.ones(self.K)
+        # # Update linearized matrices
+        # if current_time == 0:
+        #     self._update_linearized_matrices(self.A_d, self.B_d, self.h_d)
         
         # Optimization at every sampling time
-        if (time_index % self.sampling_time) == 0:
+        if (current_time % self.sampling_time) == 0:
 
-            self.x0_param.value = x0
+            self.x0_param.value = x
         
             # Solve the optimization problem
             try:
@@ -111,26 +131,26 @@ class MPController(ControllerBase):
                 except:
                     self.num_infeas += 1
                     
-                    control_output = dss.basal_handler_params['open-loop input'][time_index % self.sampling_time]
+                    control_input = self.open_loop_u[:, current_time % self.sampling_time]
 
                     if self.print_output:
-                        print(f"Forced control output: {control_output}")
-                    return control_output, dss
+                        print(f"Forced control output: {control_input}")
+                    return control_input
             
             # Problem solved
-            control_output = self.i_var.value
+            control_input = self.i_var.value
 
             # Save open-loop input
-            dss.basal_handler_params['open-loop input'] = np.repeat(self.i_var[:].value, self.discret_time)
+            self.open_loop_u = np.repeat(self.i_var[:].value, self.discret_time)
 
             if self.print_output:
-                print(f"Optimized control output: {control_output}")
-            return control_output, dss
+                print(f"Optimized control output: {control_input}")
+            return control_input
         
         # Open-loop control
         else:
-            control_output = dss.basal_handler_params['open-loop input'][time_index % self.sampling_time]
+            control_input = self.open_loop_u[current_time % self.sampling_time]
             
             if self.print_output:
-                print(f"Open-loop control output: {control_output}")
-            return control_output, dss
+                print(f"Open-loop control output: {control_input}")
+            return control_input
--- src/controllers/__init__.py
+++ src/controllers/__init__.py
@@ -4,4 +4,4 @@ from .PID_controller        import PIDController
 from .PID_controller_T_top  import PIDController_T_top
 from .LQR_controller_disc   import LQRControllerDisc
 from .LQR_controller_cont   import LQRControllerCont
-from .MPC_controller        import MPController
+from .MPC_controller        import MPCController
